ac377670537dc7edead77e914eda502155db45f7
David Dai <daviddai@Davids-MacBook-Pro.local>
2018-02-08 20:08:10 +1100

    added validation to form

--- src/app/components/heroform/heroform.component.ts
+++ src/app/components/heroform/heroform.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroformComponent implements OnInit {
 
-  constructor() { }
+  hero: Hero;
+  heroPowers: string[];
 
-  ngOnInit() {
+  constructor() {
+    this.hero = new Hero();
+    this.heroPowers = HeroPowerService.getHeroPowers();
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    console.log(JSON.stringify(this.hero));
+    this.clearForm();
+  }
+
+  clearForm() {
+    this.hero.name = "";
+    this.hero.alterEgo = "";
+    this.hero.power = "";
+  }
+
+}
+
+class Hero {
+  name: string;
+  alterEgo: string;
+  power: string;
+}
+
+class HeroPowerService {
+
+  private static heroPowers: string[] = [
+    'Really Smart',
+    'Unbelievably Strong',
+    'Fast As Light'
+  ];
+
+  static getHeroPowers() {
+    return HeroPowerService.heroPowers;
   }
 
 }
